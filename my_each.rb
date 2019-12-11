f50bda31120feef967f9bfe49390e96bd505b096
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 19:06:32 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 def my_each(array)
   # code here
-#   counter = 0 
-#   while counter < array.length 
-#   yield (array [counter])
-# i= i + 1 
+  counter = 0 
+  while counter < array.length 
+  yield (array [counter])
+i= i + 1 
 end
